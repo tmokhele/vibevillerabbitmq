6e02dd0674c8ef338dcb9b995975bc39fce63d12
Thabiso Mokhele <Thabiso.Mokhele@dstvdm.com>
2018-11-13 09:38:59 +0200

    sonar fixes

--- src/main/java/vibeville/app/util/ObjectWriterUtil.java
+++ src/main/java/vibeville/app/util/ObjectWriterUtil.java
@@ -3,12 +3,17 @@ package vibeville.app.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 
 
 public class ObjectWriterUtil {
     public static String getJsonObject(Object o) throws JsonProcessingException {
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter().with(new ISO8601DateFormat());
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter().with(new StdDateFormat());
         return ow.writeValueAsString(o);
+    }
+
+    private ObjectWriterUtil()
+    {
+        throw new IllegalStateException("ObjectWriterUtil class");
     }
 }
