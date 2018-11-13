47b16ecea46b7c3e26acaca84255f34f9bec7874
Thabiso Mokhele <Thabiso.Mokhele@dstvdm.com>
2018-11-13 15:53:37 +0200

    fixed

--- src/main/java/vibeville/app/VibeVilleRabbitClient.java
+++ src/main/java/vibeville/app/VibeVilleRabbitClient.java
@@ -1,13 +1,48 @@
 package vibeville.app;
 
+import org.springframework.amqp.core.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+import org.springframework.messaging.handler.annotation.support.MessageHandlerMethodFactory;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
 public class VibeVilleRabbitClient {
+
+    public static final String USER_REGISTRATION_REPROCESS = "reprocessing-user-registration-queue";
+    public static final String USER_REGISTRATION = "user-registration-queue";
+    private static final String EXCHANGE_USER_REGISTRATION = "user-registration-exchange";
+
+    @Bean
+    Queue userRegistrationQueue() {
+        return QueueBuilder.durable(USER_REGISTRATION)
+                .withArgument("x-dead-letter-exchange", "")
+                .withArgument("x-dead-letter-routing-key", USER_REGISTRATION_REPROCESS)
+                .build();
+    }
+    @Bean
+    Queue reprocessingUserRegistrationQueue() {
+        return QueueBuilder.durable(USER_REGISTRATION_REPROCESS).build();
+    }
+    @Bean
+    TopicExchange userRegistrationExchange() {
+        return new TopicExchange(EXCHANGE_USER_REGISTRATION);
+    }
+
+    @Bean
+    Binding bindingPublisher(Queue userRegistrationQueue, TopicExchange publisherExchange) {
+        return BindingBuilder.bind(userRegistrationQueue).to(publisherExchange).with(USER_REGISTRATION);
+    }
+
+    @Bean
+    MessageHandlerMethodFactory messageHandlerMethodFactory() {
+        return new DefaultMessageHandlerMethodFactory();
+    }
+
 
     @PostConstruct
     void init() {
