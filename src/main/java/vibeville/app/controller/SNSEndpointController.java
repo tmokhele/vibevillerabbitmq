55c76b397f56843ae9cf9b7d51435de030d20bc4
Thabiso Mokhele <Thabiso.Mokhele@dstvdm.com>
2018-11-22 17:44:35 +0200

    subcription

--- src/main/java/vibeville/app/controller/SNSEndpointController.java
+++ src/main/java/vibeville/app/controller/SNSEndpointController.java
@@ -4,21 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.aws.messaging.config.annotation.NotificationMessage;
 import org.springframework.cloud.aws.messaging.config.annotation.NotificationSubject;
+import org.springframework.cloud.aws.messaging.endpoint.NotificationStatus;
 import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationMessageMapping;
 import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationSubscriptionMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vibeville.app.model.SNSMessage;
 
 @Controller
 @RequestMapping("/topic-subscriber")
 public class SNSEndpointController {
     private static final Logger logger = LoggerFactory.getLogger(SNSEndpointController.class);
     @NotificationSubscriptionMapping
-    public void confirmUnsubscribeMessage(
-            @NotificationMessage String message,
-            @NotificationSubject String subject) {
-        logger.info("notification :"+message);
-//        notificationStatus.confirmSubscription();
+    public void confirmUnsubscribeMessage( NotificationStatus status) {
+
     }
 
     @NotificationMessageMapping
