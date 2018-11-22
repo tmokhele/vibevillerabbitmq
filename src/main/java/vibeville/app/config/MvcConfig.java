d9de5e549a08c9636116e01e42dd7e51ae00f7a0
Thabiso Mokhele <Thabiso.Mokhele@dstvdm.com>
2018-11-22 18:09:45 +0200

    configs

--- src/main/java/vibeville/app/config/MvcConfig.java
+++ src/main/java/vibeville/app/config/MvcConfig.java
@@ -1,31 +1,26 @@
 package vibeville.app.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import org.springframework.cloud.aws.messaging.endpoint.NotificationMessageHandlerMethodArgumentResolver;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.aws.messaging.endpoint.NotificationStatusHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
-public class MvcConfig extends WebMvcConfigurerAdapter {
+public class MvcConfig extends WebMvcConfigurationSupport {
 
-    @Autowired
-    private NotificationMessageHandlerMethodArgumentResolver notificationResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(notificationMessageHandlerMethodArgumentResolver());
+        argumentResolvers.add(new NotificationStatusHandlerMethodArgumentResolver(AmazonSNSClientBuilder.standard()
+                .withRegion(Regions.EU_WEST_1)
+                .build()));
     }
-
-    @Bean
-    public NotificationMessageHandlerMethodArgumentResolver notificationMessageHandlerMethodArgumentResolver () {
-
-        return new NotificationMessageHandlerMethodArgumentResolver();
-    };
 
 }
